823296083f87d69312b2dcc57ae582c84c834f82
Mat McLoughlin <mat@poweredbybeard.net>
2020-07-08 21:00:41 +0100

    enabling typemapper test

--- Scheduling.Test/TypeMapperTest.cs
+++ Scheduling.Test/TypeMapperTest.cs
@@ -1,56 +1,58 @@
 using System;
 using Newtonsoft.Json.Linq;
+using Scheduling.Domain.DoctorDay.Events;
+using Scheduling.EventSourcing;
 using Xunit;
 
 namespace Scheduling.Test
 {
     public class TypeMapperTest
     {
-        // [Fact]
-        // public void check_slot_booking_cancelled_correctly_maps_with_default_value()
-        // {
-        //     EventMappings.MapEventTypes();
-        //
-        //     var mapper = TypeMapper.GetDataToType("doctorday-slot-booking-cancelled");
-        //
-        //     var slotId = Guid.NewGuid();
-        //     var data = new JObject
-        //     {
-        //         ["dayId"] = "dayId",
-        //         ["slotId"] = slotId.ToString(),
-        //         ["reason"] = "reason",
-        //
-        //     };
-        //
-        //     var slotBookingCancelled = mapper(data) as SlotBookingCancelled;
-        //
-        //     Assert.NotNull(slotBookingCancelled);
-        //     Assert.IsType<SlotBookingCancelled>(slotBookingCancelled);
-        //     Assert.Equal(new SlotBookingCancelled("dayId", slotId, "reason", "unknown request"), slotBookingCancelled);
-        // }
-        //
-        // [Fact]
-        // public void check_slot_booking_cancelled_correctly_maps_with_value_present()
-        // {
-        //     EventMappings.MapEventTypes();
-        //
-        //     var mapper = TypeMapper.GetDataToType("doctorday-slot-booking-cancelled");
-        //
-        //     var slotId = Guid.NewGuid();
-        //     var data = new JObject
-        //     {
-        //         ["dayId"] = "dayId",
-        //         ["slotId"] = slotId.ToString(),
-        //         ["reason"] = "reason",
-        //         ["requestedBy"] = "doctor"
-        //
-        //     };
-        //
-        //     var slotBookingCancelled = mapper(data) as SlotBookingCancelled;
-        //
-        //     Assert.NotNull(slotBookingCancelled);
-        //     Assert.IsType<SlotBookingCancelled>(slotBookingCancelled);
-        //     Assert.Equal(new SlotBookingCancelled("dayId", slotId, "reason", "doctor"), slotBookingCancelled);
-        // }
+        [Fact]
+        public void check_slot_booking_cancelled_correctly_maps_with_default_value()
+        {
+            EventMappings.MapEventTypes();
+
+            var mapper = TypeMapper.GetDataToType("doctorday-slot-booking-cancelled");
+
+            var slotId = Guid.NewGuid();
+            var data = new JObject
+            {
+                ["dayId"] = "dayId",
+                ["slotId"] = slotId.ToString(),
+                ["reason"] = "reason",
+
+            };
+
+            var slotBookingCancelled = mapper(data) as SlotBookingCancelled;
+
+            Assert.NotNull(slotBookingCancelled);
+            Assert.IsType<SlotBookingCancelled>(slotBookingCancelled);
+            Assert.Equal(new SlotBookingCancelled("dayId", slotId, "reason", "unknown request"), slotBookingCancelled);
+        }
+
+        [Fact]
+        public void check_slot_booking_cancelled_correctly_maps_with_value_present()
+        {
+            EventMappings.MapEventTypes();
+
+            var mapper = TypeMapper.GetDataToType("doctorday-slot-booking-cancelled");
+
+            var slotId = Guid.NewGuid();
+            var data = new JObject
+            {
+                ["dayId"] = "dayId",
+                ["slotId"] = slotId.ToString(),
+                ["reason"] = "reason",
+                ["requestedBy"] = "doctor"
+
+            };
+
+            var slotBookingCancelled = mapper(data) as SlotBookingCancelled;
+
+            Assert.NotNull(slotBookingCancelled);
+            Assert.IsType<SlotBookingCancelled>(slotBookingCancelled);
+            Assert.Equal(new SlotBookingCancelled("dayId", slotId, "reason", "doctor"), slotBookingCancelled);
+        }
     }
 }
