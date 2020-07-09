83c72226a49c297b404fd7c56b92fade7e0aefe3
Kacper Gunia <kacper@gunia.me>
2020-07-09 21:35:53 +0100

    Setup process managers exercise

--- Scheduling/Application/OverbookingProcessManager.cs
+++ Scheduling/Application/OverbookingProcessManager.cs
@@ -15,31 +15,17 @@ namespace Scheduling.Domain.Application
         {
             When<SlotScheduled>(async e =>
             {
-                await bookedSlotRepository.AddSlot(new BookedSlot
-                {
-                    Id = e.SlotId.ToString(),
-                    Month = e.SlotStartTime.Month,
-                    DayId = e.DayId
-                });
+                
             });
 
             When<SlotBooked>(async e =>
             {
-                await bookedSlotRepository.MarkSlotAsBooked(e.SlotId.ToString(), e.PatientId);
 
-                var slot = await bookedSlotRepository.GetSlot(e.SlotId.ToString());
-                var count = await bookedSlotRepository.CountByPatientAndMonth(e.PatientId, slot.Month);
-
-                if (count > bookingLimitedPerPatient)
-                {
-                    await commandStore.Send(new CancelSlotBooking(e.DayId, e.SlotId, "overbooked"),
-                        new CommandMetadata(new CorrelationId(idGenerator()), new CausationId(idGenerator())));
-                }
             });
 
             When<SlotBookingCancelled>(async e =>
             {
-                await bookedSlotRepository.MarkSlotAsAvailable(e.SlotId.ToString());
+                
             });
         }
     }
--- Scheduling/Program.cs
+++ Scheduling/Program.cs
@@ -58,8 +58,6 @@ namespace Scheduling
             CreateHostBuilder(args).Build().Run();
         }
 
-
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
