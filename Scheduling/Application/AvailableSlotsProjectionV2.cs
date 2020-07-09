fb65ccd69ee34f713da16e7725ff24a1ab17dc0f
Kacper Gunia <kacper@gunia.me>
2020-07-09 21:25:59 +0100

    Setup projections exercise

--- Scheduling/Application/AvailableSlotsProjectionV2.cs
+++ Scheduling/Application/AvailableSlotsProjectionV2.cs
@@ -9,25 +9,12 @@ namespace Scheduling.Application
     {
         public AvailableSlotsProjectionV2(MongoDbAvailableSlotsRepositoryV2 availableSlotsRepository)
         {
-            When<SlotScheduled>(e =>
-                availableSlotsRepository.AddSlot(new MongoDbAvailableSlotV2
-                {
-                    Id = e.SlotId.ToString(),
-                    DayId = e.DayId,
-                    Duration = e.SlotDuration,
-                    Date = e.SlotStartTime.ToString("dd-MM-yyyy"),
-                    StartTime = e.SlotStartTime.ToString("h:mm tt"),
-                    IsBooked = false
-                }));
+            When<SlotScheduled>(e => { return null;});
 
-            When<SlotBooked>(e =>
-                availableSlotsRepository.HideSlot(e.SlotId));
-
-            When<SlotBookingCancelled>(e =>
-                availableSlotsRepository.ShowSlot(e.SlotId));
-
-            When<SlotScheduleCancelled>(e =>
-                availableSlotsRepository.DeleteSlot(e.SlotId));
+            When<SlotBooked>(e => { return null;});
+            
+            // Add when for SlotBookingCancelled
+            
         }
     }
 }
