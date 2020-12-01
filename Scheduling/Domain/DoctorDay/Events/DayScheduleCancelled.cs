44249ad72dde4244834642e33691427286293fed
Mat McLoughlin <mat@poweredbybeard.net>
2020-12-01 19:46:04 +0000

    Improving test to check value equality

--- Scheduling/Domain/DoctorDay/Events/DayScheduleCancelled.cs
+++ Scheduling/Domain/DoctorDay/Events/DayScheduleCancelled.cs
@@ -1,3 +1,4 @@
+using System;
 using Scheduling.EventSourcing;
 
 namespace Scheduling.Domain.DoctorDay.Events
@@ -9,6 +10,24 @@ namespace Scheduling.Domain.DoctorDay.Events
         public DayScheduleCancelled(string dayId)
         {
             DayId = dayId;
+        }
+
+        protected bool Equals(DayScheduleCancelled other)
+        {
+            return base.Equals(other) && DayId == other.DayId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DayScheduleCancelled) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), DayId);
         }
     }
 }
