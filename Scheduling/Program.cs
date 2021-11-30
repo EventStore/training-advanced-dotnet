31a3f2ec64b64231ec77cd15551dee55c011d26f
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-11-30 15:48:13 +0100

    Updated to new single-file Program.cs
    # Conflicts:
    #	Scheduling/Program.cs

--- Scheduling/Program.cs
+++ Scheduling/Program.cs
@@ -1,65 +1,89 @@
 using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using EventStore.Client;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using MongoDB.Driver;
+using Scheduling;
 using Scheduling.Application;
 using Scheduling.Domain.DoctorDay.Events;
+using Scheduling.Domain.ReadModel;
+using Scheduling.EventSourcing;
 using Scheduling.Infrastructure.EventStore;
 using Scheduling.Infrastructure.InMemory;
 using Scheduling.Infrastructure.MongoDb;
 using Scheduling.Infrastructure.Projections;
 
-namespace Scheduling
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddControllers().AddNewtonsoftJson();
+
+var mongoClient = new MongoClient("mongodb://localhost");
+
+var client = Helpers.GetEventStoreClient();
+var eventStore = new EsEventStore(client, Helpers.Tenant);
+
+services.AddSingleton<IEventStore>(eventStore);
+services.AddSingleton(Helpers.GetDispatcher(eventStore));
+services.AddSingleton(_ => mongoClient.GetDatabase("projections"));
+services.AddSingleton<IAvailableSlotsRepository, MongoDbAvailableSlotsRepository>();
+services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            EventMappings.MapEventTypes();
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+            { Title = "Patients API", Description = "API for booking doctor appointments", Version = "v1" });
+});
 
-            var client = Helpers.GetEventStoreClient();
-            var esStore = new EsEventStore(client, Helpers.Tenant);
+EventMappings.MapEventTypes();
 
-            var mongoClient = new MongoClient("mongodb://localhost");
+var mongoDatabase = mongoClient.GetDatabase("projections");
+var availableSlotsRepository = new MongoDbAvailableSlotsRepository(mongoDatabase);
 
-            var mongoDatabase = mongoClient.GetDatabase("projections");
-            var availableSlotsRepository = new MongoDbAvailableSlotsRepository(mongoDatabase);
+var dispatcher = Helpers.GetDispatcher(eventStore);
 
-            var dispatcher = Helpers.GetDispatcher(esStore);
+var commandStore = new EsCommandStore(eventStore, client, dispatcher, Helpers.Tenant);
 
-            var commandStore = new EsCommandStore(esStore, client, dispatcher, Helpers.Tenant);
+var dayArchiverProcessManager = new DayArchiverProcessManager(
+    new InMemoryColdStorage(),
+    new MongoDbArchivableDaysRepository(mongoDatabase),
+    commandStore,
+    TimeSpan.FromDays(-180),
+    eventStore,
+    Guid.NewGuid);
 
-            var dayArchiverProcessManager = new DayArchiverProcessManager(
-                new InMemoryColdStorage(),
-                new MongoDbArchivableDaysRepository(mongoDatabase),
-                commandStore,
-                TimeSpan.FromDays(-180),
-                esStore,
-                Guid.NewGuid);
+var subManager = new SubscriptionManager(
+    client,
+    new EsCheckpointStore(client, "DaySubscription"),
+    "DaySubscription",
+    StreamName.AllStream,
+    new Projector(
+        new AvailableSlotsProjection(availableSlotsRepository)
+    ),
+    new Projector(
+        dayArchiverProcessManager)
+);
 
-            var subManager = new SubscriptionManager(
-                client,
-                new EsCheckpointStore(client, "DaySubscription"),
-                "DaySubscription",
-                StreamName.AllStream,
-                new Projector(
-                    new AvailableSlotsProjection(availableSlotsRepository)
-                ),
-                new Projector(
-                    dayArchiverProcessManager)
-            );
+await subManager.Start();
+await commandStore.Start();
 
-            await subManager.Start();
-            await commandStore.Start();
+var app = builder.Build();
 
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Patients API"); });
+
+app.Run();
