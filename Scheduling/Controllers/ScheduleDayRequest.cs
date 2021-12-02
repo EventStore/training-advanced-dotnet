92d7242f00ea8c0e4a42a403d0da5d94fac16cdc
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-12-02 10:34:23 +0100

    Changed requests to records
    # Conflicts:
    #	Scheduling.Test/OverbookingProcessManagerTest.cs
    #	Scheduling/Application/OverbookingProcessManager.cs
    #	Scheduling/Controllers/ApiController.cs
    #	Scheduling/Controllers/ScheduleDayRequest.cs
    #	Scheduling/Domain/ReadModel/BookedSlot.cs
    #	Scheduling/Domain/ReadModel/IBookedSlotsRepository.cs
    #	Scheduling/Infrastructure/MongoDb/MongoDbBookedSlotRepository.cs

--- Scheduling/Controllers/ScheduleDayRequest.cs
+++ Scheduling/Controllers/ScheduleDayRequest.cs
@@ -5,25 +5,21 @@ using Scheduling.Domain.DoctorDay.Commands;
 
 namespace Scheduling.Controllers
 {
-    public class ScheduleDayRequest
+    public record ScheduleDayRequest(
+        Guid DoctorId,
+        DateTime Date,
+        List<SlotRequest> Slots
+    )
     {
-        public Guid DoctorId { get; set; }
-
-        public DateTime? Date { get; set; }
-
-        public List<SlotRequest> Slots { get; set; }
-
         public ScheduleDay ToCommand() =>
             new ScheduleDay(
                 DoctorId,
-                Date.Value,
-                Slots.Select(s => new ScheduledSlot(s.Duration.Value, s.StartTime.Value)).ToList());
+                Date,
+                Slots.Select(s => new ScheduledSlot(s.Duration, s.StartTime)).ToList());
     }
 
-    public class SlotRequest
-    {
-        public TimeSpan? Duration { get; set; }
-
-        public DateTime? StartTime { get; set; }
-    }
+    public record SlotRequest(
+        TimeSpan Duration,
+        DateTime StartTime
+    );
 }
